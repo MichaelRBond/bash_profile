8788ec41b3dfeb472e8edcd252603d1da5ed10f0
Michael Bond <MichaelRBond@users.noreply.github.com>
2024-11-08 05:41:19 -0500

    vscode settings

---
+++ vscode/extensions.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+ln -s "${HOME}/Documents/z.setup/bash_profile/vscode/settings.json" "${HOME}/Library/Application Support/Code/User/settings.json"
+ln -s "${HOME}/Documents/z.setup/bash_profile/vscode/keybindings.json" "${HOME}/Library/Application Support/Code/User/keybindings.json"
+
+code --install-extension bluebrown.yamlfmt
+code --install-extension davidanson.vscode-markdownlint
+code --install-extension editorconfig.editorconfig
+code --install-extension enkia.tokyo-night
+code --install-extension github.copilot
+code --install-extension github.copilot-chat
+code --install-extension github.vscode-github-actions
+code --install-extension inferrinizzard.prettier-sql-vscode
+code --install-extension marp-team.marp-vscode
+code --install-extension ms-python.debugpy
+code --install-extension ms-python.python
+code --install-extension ms-python.vscode-pylance
+code --install-extension ms-toolsai.jupyter
+code --install-extension ms-toolsai.jupyter-keymap
+code --install-extension ms-toolsai.jupyter-renderers
+code --install-extension ms-toolsai.vscode-jupyter-cell-tags
+code --install-extension ms-toolsai.vscode-jupyter-slideshow
+code --install-extension redhat.vscode-yaml
+code --install-extension streetsidesoftware.code-spell-checker
+code --install-extension timonwong.shellcheck
+code --install-extension wayou.vscode-todo-highlight
